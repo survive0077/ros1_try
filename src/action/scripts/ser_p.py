5291a019f4d2bd38f3f3417c1fd28f654b3dba5f
xjy <YXJ20001222@gmail.com>
2023-01-06 16:17:49 +0800

    -a

--- src/action/scripts/ser_p.py
+++ src/action/scripts/ser_p.py
@@ -1,8 +1,34 @@
-import rospy
-from actionlib import simple_action_server
-from action.scripts import 
+#! /usr/bin/env ppython
 
+import rospy
+from actionlib import SimpleActionServer
+from action.msg import addintsAction, addintsFeedback, addintsResult
+
+
+class myAction:
+    def __init__(self):
+        # 关闭自动启动
+        self.server = SimpleActionServer("addints", addintsAction, self.callback, False)
+        self.server.start()
+        rospy.loginfo("Server start")
+    
+    def callback(self, goal):
+        goal_num = goal.num
+        rospy.loginfo("The target number is %d", goal_num)
+        rate = rospy.Rate(10)
+        sum = 0
+        for i in range(1, goal_num + 1):
+            sum = sum + i
+            rate.sleep()
+            fb = addintsFeedback()
+            fb.percent = i / goal_num
+            self.server.publish_feedback(fb)
+        result = addintsResult()
+        result.result = sum
+        self.server.set_succeeded(result)
 
 if __name__ == "__main__":
     rospy.init_node("ser_p")
-    ser = simple_action_server("addints", )
+    ser = myAction()
+    rospy.spin()
+    
